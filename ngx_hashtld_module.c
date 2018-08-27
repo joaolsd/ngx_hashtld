1fc9c8d2ff6ba34ff10f4627912d4e941fcf9b54
Joao Damas <joao@bondis.org>
2018-08-27 18:07:08 +0200

    Last round of bugs?

--- ngx_hashtld_module.c
+++ ngx_hashtld_module.c
@@ -2,15 +2,16 @@
 #include <ngx_http.h>
 #include <ngx_http_variables.h>
 
-char *test_domains;
+#define MAX_DOMAINS 512
+char *test_domains[MAX_DOMAINS];
 int num_domains;
 
-ngx_conf_t *my_cf
+ngx_conf_t *my_cf;
 
 int djb2_hash(char *str, int num_buckets);
 
 static ngx_int_t
-ngx_hashgtld_get(ngx_http_request_t *r, ngx_http_variable_value_t *v, \
+ngx_hashtld_get(ngx_http_request_t *r, ngx_http_variable_value_t *v, \
         uintptr_t data) {
           
     char *exp_str;
@@ -30,17 +31,31 @@ ngx_hashgtld_get(ngx_http_request_t *r, ngx_http_variable_value_t *v, \
     ngx_int_t txad_idx;
 
     ngx_http_variable_value_t *txrnd_val;
-    ngx_http_variable_value_t *txccid_val;
+    ngx_http_variable_value_t *ccid_val;
     ngx_http_variable_value_t *txsec_val;
     ngx_http_variable_value_t *txad_val;
+    
+    ngx_str_t str_txrnd;
+    ngx_str_t str_ccid;
+    ngx_str_t str_txsec;
+    ngx_str_t str_txad;
+    
+    str_txrnd.data = (u_char *)"txrnd";
+    str_txrnd.len = strlen((const char *)str_txrnd.data);
+    str_ccid.data = (u_char *)"ccid";
+    str_ccid.len = strlen((const char *)str_ccid.data);
+    str_txsec.data = (u_char *)"txsec";
+    str_txsec.len = strlen((const char *)str_txsec.data);
+    str_txad.data = (u_char *)"txad";
+    str_txad.len = strlen((const char *)str_txad.data);
 
-    txrnd_idx = ngx_http_get_variable_index(my_cf, "txrnd");
-    txccid_idx = ngx_http_get_variable_index(my_cf, "ccid");
-    txsec_idx = ngx_http_get_variable_index(my_cf, "txsec");
-    txad_idx = ngx_http_get_variable_index(my_cf, "txad");
+    txrnd_idx = ngx_http_get_variable_index(my_cf, &str_txrnd);
+    ccid_idx = ngx_http_get_variable_index(my_cf, &str_ccid);
+    txsec_idx = ngx_http_get_variable_index(my_cf, &str_txsec);
+    txad_idx = ngx_http_get_variable_index(my_cf, &str_txad);
     
     txrnd_val = ngx_http_get_indexed_variable(r, txrnd_idx);
-    txccid_val = ngx_http_get_indexed_variable(r, ccid_idx);
+    ccid_val = ngx_http_get_indexed_variable(r, ccid_idx);
     txsec_val = ngx_http_get_indexed_variable(r, txsec_idx);
     txad_val = ngx_http_get_indexed_variable(r, txad_idx);
     
@@ -49,7 +64,7 @@ ngx_hashgtld_get(ngx_http_request_t *r, ngx_http_variable_value_t *v, \
     exp_str = strcpy(exp_str, "6du-u");
     exp_str = strncat(exp_str, (char *)txrnd_val->data, txrnd_val->len);
     exp_str = strcat(exp_str, "-c");
-    exp_str = strncat(exp_str, (char *)txccid_val->data, txccid_val->len);
+    exp_str = strncat(exp_str, (char *)ccid_val->data, ccid_val->len);
     exp_str = strcat(exp_str, "-s");
     exp_str = strncat(exp_str, (char *)txsec_val->data, txsec_val->len);
     exp_str = strcat(exp_str, "-i");
@@ -60,13 +75,6 @@ ngx_hashgtld_get(ngx_http_request_t *r, ngx_http_variable_value_t *v, \
     tld_index = djb2_hash(exp_str, num_domains);
     // Use the hash to look up the gTLD to issue
     new_domain = test_domains[tld_index];
-    
-    u_char *out = ngx_pnalloc(r->pool, enclen);
-    if (out == NULL) {
-        v->valid = 0;
-        v->not_found = 1;
-        return NGX_ERROR;
-    }
 
     v->len = strlen(new_domain);
     v->data = (u_char *)new_domain;
@@ -91,32 +99,42 @@ int djb2_hash(char *str, int num_buckets) {
 }
 
 int read_test_domains(char *domain_list, ngx_cycle_t *cycle) {
+  FILE *f_dl;
   // If there is a domain list to load, load it
   if (strlen(domain_list) !=0 ) {
-  	FILE *f_dl = fopen(domain_list, "r");
-  	if (f_dl == NULL) {
+    f_dl = fopen(domain_list, "r");
+    if (f_dl == NULL) {
       ngx_log_error(NGX_LOG_ERR, cycle->log, 0,
                     "Could not read domain list %s", domain_list);
       return NGX_ERROR;
-  	}
-  	// Read the domain list
-  	int max_line_len = 128;
-  	char *line_buffer = (char *)calloc(max_line_len, 1);
-  	num_domains = 0;
-  	while ( (line_buffer = fgets(line_buffer, max_line_len, f_dl)) != NULL) {
-  		line_buffer[strlen(line_buffer)-1] = '\0';
-  		test_domains[num_domains] = strdup(line_buffer);
-  		num_domains++;
-  	}
-  	// for (int i=0; i<num_domains;i++) {
-  	// 	printf("%s\n",test_domains[i]);
-  	// }
-    return num_domains;
+    }
+  } else {
+    ngx_log_error(NGX_LOG_ERR, cycle->log, 0,
+                  "domain list file name is empty");
+    return NGX_ERROR;
   }
+  // Read the domain list
+  int max_line_len = 128;
+  char *line_buffer = (char *)calloc(max_line_len, 1);
+  num_domains = 0;
+  while ( (line_buffer = fgets(line_buffer, max_line_len, f_dl)) != NULL) {
+    line_buffer[strlen(line_buffer)-1] = '\0';
+    test_domains[num_domains] = strdup(line_buffer);
+    num_domains++;
+    if (num_domains > MAX_DOMAINS) {
+      ngx_log_error(NGX_LOG_ERR, cycle->log, 0,
+                    "Too many test domains. Maximum is %d", MAX_DOMAINS);
+      return NGX_ERROR;
+    }
+  }
+  // for (int i=0; i<num_domains;i++) {
+  // 	printf("%s\n",test_domains[i]);
+  // }
+  return num_domains;
 }
 
 static ngx_int_t
-ngx_hashgtld_init_process(ngx_cycle_t *cycle) {
+ngx_hashtld_init_process(ngx_cycle_t *cycle) {
     // Load list of gTLDs from file
     char *domain_list = "/usr/local/dns/domain_list.txt";
     num_domains = read_test_domains(domain_list, cycle);
@@ -128,31 +146,31 @@ ngx_hashgtld_init_process(ngx_cycle_t *cycle) {
 }
 
 static void
-ngx_hashgtld_exit_process(ngx_cycle_t *cycle) {
+ngx_hashtld_exit_process(ngx_cycle_t *cycle) {
   return;
 }
 
-static ngx_str_t ngx_hashgtld_variable_name = ngx_string("hashgtld");
+static ngx_str_t ngx_hashtld_variable_name = ngx_string("hashtld");
 
-static ngx_int_t ngx_hashgtld_add_variables(ngx_conf_t *cf)
+static ngx_int_t ngx_hashtld_add_variables(ngx_conf_t *cf)
 {
   my_cf = cf;
   ngx_http_variable_t* var = ngx_http_add_variable(
           cf,
-          &ngx_hashgtld_variable_name,
+          &ngx_hashtld_variable_name,
           NGX_HTTP_VAR_NOHASH);
 
   if (var == NULL) {
       return NGX_ERROR;
   }
 
-  var->get_handler = ngx_hashgtld_get;
+  var->get_handler = ngx_hashtld_get;
 
   return NGX_OK;
 }
 
-static ngx_http_module_t  ngx_hashgtld_module_ctx = {
-  ngx_hashgtld_add_variables,     /* preconfiguration */
+static ngx_http_module_t  ngx_hashtld_module_ctx = {
+  ngx_hashtld_add_variables,     /* preconfiguration */
   NULL,                        /* postconfiguration */
 
   NULL,        /* create main configuration */
@@ -165,21 +183,21 @@ static ngx_http_module_t  ngx_hashgtld_module_ctx = {
   NULL         /* merge location configuration */
 };
 
-static ngx_command_t  ngx_hashgtld_module_commands[] = {
+static ngx_command_t  ngx_hashtld_module_commands[] = {
   ngx_null_command
 };
 
-ngx_module_t  ngx_hashgtld_module = {
+ngx_module_t  ngx_hashtld_module = {
   NGX_MODULE_V1,
-  &ngx_hashgtld_module_ctx,      /* module context */
-  ngx_hashgtld_module_commands,  /* module directives */
+  &ngx_hashtld_module_ctx,      /* module context */
+  ngx_hashtld_module_commands,  /* module directives */
   NGX_HTTP_MODULE,                /* module type */
   NULL,                           /* init master */
   NULL,                           /* init module */
-  ngx_hashgtld_init_process,          /* init process */
+  ngx_hashtld_init_process,          /* init process */
   NULL,                           /* init thread */
   NULL,                           /* exit thread */
-  ngx_hashgtld_exit_process,          /* exit process */
+  ngx_hashtld_exit_process,          /* exit process */
   NULL,                           /* exit master */
   NGX_MODULE_V1_PADDING
 };
